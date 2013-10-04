8ec88af278f2f97a20eeb89f3377cabdd954b5aa
Jiri Balaty <jiri.balaty@gmail.com>
2013-10-04 12:42:50 +0200

    Small change in email template

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,7 @@ puts 'Processing config file'
 # Initialize the Rails application.
 RailsTestApp::Application.initialize!
 RailsTestApp::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'localhost',
-      port: 25,
-      enable_starttls_auto: false
-  }
+  config.action_mailer.delivery_method = :test
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'info@pcin.cz'}
 end
